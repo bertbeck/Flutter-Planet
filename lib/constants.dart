9b720295094c35dee3faa38acba893094104f220
ashi31 <riyaashita@gmail.com>
2020-06-28 23:56:00 +0530

    constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kAppMessage =
+    "Welcome to Flutter Planet - the app that commits to save the planet! Please make a commitment and share with the world!";
+
+const kAppbarHeight = 350.0;
+const kHeaderStyle = TextStyle(color: kTextColour, fontSize: 20.0);
+const kTextColour = Color.fromARGB(230, 10, 32, 47);
