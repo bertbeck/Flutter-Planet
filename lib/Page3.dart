b67a9d840e9f95d203a400bfe89af7940167f4ab
ashi31 <riyaashita@gmail.com>
2020-06-29 15:02:44 +0530

    Connected page 2 and page 3

--- lib/Page3.dart
+++ lib/Page3.dart
@@ -1,101 +1,119 @@
 import 'package:flutter/material.dart';
 
-import 'Page4.dart';
+import './page2/page2.dart';
+
 class Page3 extends StatefulWidget {
   @override
   _Page3State createState() => _Page3State();
 }
 
 class _Page3State extends State<Page3> {
-  List<String> _locations = ['one', 'two', 'three'];
-   TextEditingController descriptionController = TextEditingController();
-   String _selectedLocations;
-
+  List<String> tags = [
+    'Plant',
+    'EndRacism',
+    'HelpOlder',
+    "SaveWater",
+    "SaveElectricity",
+    "NoToPlastics"
+  ];
+  TextEditingController descriptionController = TextEditingController();
+  String _selectedLocations;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Commitments'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-         Container(
-           padding: EdgeInsets.only(top: 80,left: 15 , right: 15),
-           child: Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 70,
-            width: 150,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
+        appBar: AppBar(
+          title: Text('Add New Commitment'),
+          backgroundColor: Colors.green,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 80, left: 15, right: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        height: 70,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            DropdownButton<String>(
+                                value: _selectedLocations,
+                                icon: Icon(Icons.arrow_drop_down),
+                                iconSize: 35,
+                                elevation: 16,
+                                style: TextStyle(color: Colors.black),
+                                hint: Text('Commitments'),
+                                underline: Container(
+                                  height: 1,
+                                  color: Colors.black,
+                                ),
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    _selectedLocations = newValue;
+                                  });
+                                },
+                                items: tags.map((location) {
+                                  return DropdownMenuItem(
+                                    child: new Text(location),
+                                    value: location,
+                                  );
+                                }).toList()),
+                          ],
+                        )),
+//                    Padding(padding: EdgeInsets.only(right: 20, left: 40)),
+//                    RaisedButton(
+//                        onPressed: () {},
+//                        child: Text(
+//                          'Add',
+//                          style: TextStyle(color: Colors.white),
+//                        ),
+//                        color: Colors.black),
+                  ],
+                ),
               ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                DropdownButton<String>(
-     value: _selectedLocations,
-      icon: Icon(Icons.arrow_drop_down),
-      iconSize: 35,
-      elevation: 16,
-      style: TextStyle(color: Colors.black),
-      hint: Text('Commitments'),
-      underline: Container(
-        height: 1,
-        color: Colors.black,
-      ),
-      onChanged: (String newValue) {
-        setState(() {
-          _selectedLocations = newValue ;
-        });
-      },
-   items: _locations.map((location) {
-              return DropdownMenuItem(
-                child: new Text(location),
-                value: location,
-              );
-            }).toList()
-),
-              ],
-            )
-          ),
-           Padding(padding: EdgeInsets.only(right: 20 , left: 40)),
-            RaisedButton(onPressed: (){},
-            child: Text('Add',style: TextStyle(color: Colors.white),),
-            color: Colors.black),
-        ],
-      ),
-         ),
-        Container(
-           padding: EdgeInsets.only(left: 20 , right: 20 , top: 20 ),
-           height: 250,
-           width: 350,
-            child: TextField(
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                height: 250,
+                width: 350,
+                child: TextField(
                     controller: descriptionController,
                     decoration: InputDecoration(
                       border: new OutlineInputBorder(
-                    borderSide: new BorderSide(color: Colors.black)),
+                          borderSide: new BorderSide(color: Colors.black)),
                     ),
-                     keyboardType: TextInputType.multiline,
-                      maxLines: 8
-                  ),
-        ),
-       RaisedButton(
-         onPressed: (){
-            Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => Page4()));
-         },
-          child: Text('Add Video',style: TextStyle(color: Colors.white),),
-          color: Colors.black,
-          )
-      ],
-      ),
-      )
-    );
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 8),
+              ),
+              RaisedButton(
+                onPressed: () {
+//                  Navigator.push(context,
+//                      MaterialPageRoute(builder: (context) => Page4()));
+                  dummy.add(
+                    Commitment(
+                        tag: _selectedLocations,
+                        commitment: descriptionController.text,
+                        status: "doing"),
+                  );
+                  print(dummy.length);
+                  setState(() {});
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.black,
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/page2/page2.dart
+++ lib/page2/page2.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_planet/models/commitment.dart';
 import 'package:flutter_planet/models/shoutout.dart';
 
 import '../utils.dart';
 
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
+  @override
+  _Page2State createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
   @override
   Widget build(BuildContext context) {
+    setState(() {});
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Bert's Page"),
+          backgroundColor: Colors.green,
           bottom: TabBar(
             tabs: [
               Tab(
@@ -20,7 +26,6 @@ class Page2 extends StatelessWidget {
               Tab(
                 text: 'Shout Outs',
               ),
-
             ],
           ),
         ),
@@ -35,20 +40,20 @@ class Page2 extends StatelessWidget {
   }
 }
 
-class Page2Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _Commitments(),
-        SizedBox(
-          height: 24,
-        ),
-        _ShoutOuts(),
-      ],
-    );
-  }
-}
+//class Page2Body extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Column(
+//      children: [
+//        _Commitments(),
+//        SizedBox(
+//          height: 24,
+//        ),
+//        _ShoutOuts(),
+//      ],
+//    );
+//  }
+//}
 
 class _ShoutOuts extends StatelessWidget {
   List<ShoutOut> shoutouts = ShoutOut.dummy();
@@ -61,7 +66,7 @@ class _ShoutOuts extends StatelessWidget {
         return ListTile(
           title: Text(
             s.comment,
-            style: TextStyle(fontStyle: FontStyle.italic),
+            style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0),
           ),
           subtitle: Text('- ${s.name}'),
         );
@@ -72,31 +77,92 @@ class _ShoutOuts extends StatelessWidget {
   }
 }
 
-class _Commitments extends StatelessWidget {
-  List<Commitment> commitments = Commitment.dummy();
+class _Commitments extends StatefulWidget {
+  @override
+  __CommitmentsState createState() => __CommitmentsState();
+}
 
+class __CommitmentsState extends State<_Commitments> {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (_, index) {
-        final c = commitments[index];
-        return ListTile(
-          title: Text(
-            '${c.commitment}',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-          subtitle: Row(
-            children: [
-              Chip(label: Text(c.tag, style: Theme.of(context).accentTextTheme.subtitle1,), backgroundColor: colorFor(c.tag),),
-              Spacer(),
-              Chip(label: Text(c.status), backgroundColor: c.fromStatus(),),
-            ],
-          ),
-        );
-      },
-      itemCount: commitments.length,
-      separatorBuilder: (_, __) => Divider(),
+    List<Commitment> commitments = dummy;
+    setState(() {});
+    return Scaffold(
+      body: ListView.separated(
+        itemBuilder: (_, index) {
+          final c = commitments[index];
+          return ListTile(
+            title: Text(
+              '${c.commitment}',
+              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0),
+            ),
+            subtitle: Row(
+              children: [
+                Chip(
+                  label: Text(
+                    c.tag,
+                    style: Theme.of(context).accentTextTheme.subtitle1,
+                  ),
+                  backgroundColor: colorFor(c.tag),
+                ),
+                Spacer(),
+                Chip(
+                  label: Text(c.status),
+                  backgroundColor: c.fromStatus(),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: commitments.length,
+        separatorBuilder: (_, __) => Divider(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            Navigator.of(context).pushNamed("/page3");
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
     );
   }
 }
 
+class Commitment {
+  final String commitment;
+  final String tag;
+  final String status;
+
+  Commitment({this.commitment, this.tag, this.status});
+
+  Color fromStatus() {
+    if (status == 'doing') {
+      return Colors.orangeAccent;
+    } else {
+      return Colors.greenAccent;
+    }
+  }
+
+  factory Commitment.fromMap(Map<String, dynamic> map) {
+    return Commitment(commitment: 'Commitment', tag: 'tag', status: 'status');
+  }
+}
+
+List<Commitment> dummy = [
+  Commitment(commitment: 'End racism', tag: 'EndRacism', status: 'doing'),
+  Commitment(commitment: 'Plant a tree', tag: 'Plant', status: 'done'),
+  Commitment(
+      commitment: 'Make a new friend', tag: 'NewFriend', status: 'doing'),
+  Commitment(
+      commitment: 'Help an older person', tag: 'HelpOlder', status: 'done'),
+  Commitment(
+      commitment: 'Stop using plastic bags',
+      tag: 'StopPlastic',
+      status: 'doing'),
+  Commitment(
+      commitment: 'Use ECO friendly lawn care products',
+      tag: 'EcoLawn',
+      status: 'done'),
+];
